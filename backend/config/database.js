1dc39e1857e08f791ee146bea97ead606acf56f9
Elena Sacramento <alu0100884524@ull.edu.es>
2021-01-18 21:16:54 +0000

    Conectando la aplicacion a Mongo Atlas

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,21 +1,24 @@
-const fs = require('fs');
-const file = `/home/usuario/.deployment.env`;
+// const fs = require('fs');
+// const file = `/home/usuario/.deployment.env`;
 
-const deploymentEnv = fs.readFileSync(file, 'utf8');
-let lines = deploymentEnv.split('\n');
-for (var line of lines) {
-  let splited = line.split(' = ');
-  process.env[splited[0]] = splited[1].replace(/\"/g, "");
-}
+// const deploymentEnv = fs.readFileSync(file, 'utf8');
+// let lines = deploymentEnv.split('\n');
+// for (var line of lines) {
+//   let splited = line.split(' = ');
+//   process.env[splited[0]] = splited[1].replace(/\"/g, "");
+// }
 
-const databaseHost = process.env.DATABASE_HOST;
-const databasePort = process.env.DATABASE_PORT;
-const databaseUser = process.env.DATABASE_USER;
-const databasePassword = process.env.DATABASE_PASSWORD;
-const databaseName = process.env.DATABASE_NAME;
-const databaseConnectionOpts = process.env.DATABASE_CONNECTION_OPTIONS;
+// const databaseHost = process.env.DATABASE_HOST;
+// const databasePort = process.env.DATABASE_PORT;
+// const databaseUser = process.env.DATABASE_USER;
+// const databasePassword = process.env.DATABASE_PASSWORD;
+// const databaseName = process.env.DATABASE_NAME;
+// const databaseConnectionOpts = process.env.DATABASE_CONNECTION_OPTIONS;
 
 module.exports = {
-  remoteUrl: `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?${databaseConnectionOpts}`,
+  remoteUrl: 'mongodb+srv://batman:<brucewayne>@cluster0.mmzwj.mongodb.net/<Cluster0>?retryWrites=true&w=majority',
+  //remoteUrl: `mongodb://${databaseUser}:${databasePassword}@${databaseHost}:${databasePort}/${databaseName}?${databaseConnectionOpts}`,
   localUrl: 'mongodb://localhost/gamingit'
 };
+
+
