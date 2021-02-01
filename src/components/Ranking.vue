79f3311ef6bb3e33cb234185738386420db22c79
Adan de la Rosa Lugo <alu0100909012@ull.edu.es>
2021-02-01 22:24:21 +0000

    Ranking de usuario terminado

--- src/components/Ranking.vue
+++ src/components/Ranking.vue
@@ -13,9 +13,6 @@
     <div id="gamesRankings">
     </div>
 
-
-    <!--<button @click="allGamesRankings"> Mostrar rankings </button>-->
-
   </div>
 </template>
 
@@ -27,108 +24,59 @@ export default {
   data() {
 		return {
       gamesName: [],
-      tictactoe: [], //Paula te va a cambiar el nombre en el futuro :),
       gamesRankings: [],
       userGamesRankings: []
 		}
   },
-
   created: async function () {
+    await this.getGames();
 
-    /*****************************************************
-    RANKINGS POR JUEGO
-    *****************************************************/
-    await fetch(`/games/`)
-      .then(res => {
-        return res.json();
-      })
-      .then(data => {
-        for (let x of data.response[0].allGames)
-          this.gamesName.push(x.name); 
-      })
-      .catch(err => console.log(err));
+    // RANKINGS POR JUEGO
+    await this.getRankingsByGame();
+    for (let x in this.gamesName)
+      this.ranking( this.gamesName[x], x, false);
 
-    // Bucle aquí para hacer un get de cada juego
-    for (let y in this.gamesName) {
-      this.gamesRankings.push([]);
-      await fetch(`/rankings/${this.gamesName[y]}`)
-        .then(res => {
-          if (res.status != 200)  // Si recibe 404 es que no hay datos de ese ranking
-            return res.status;
-          return res.json();
-        })
-        .then(data => {
-          if (data == 404)
-            console.log("No hay ranking de este juego");
-          else {
-            for (let x of data.response[0].ranking) {
-              this.gamesRankings[y].push(x);
-            }
-          }
-        })
-        .catch(err => console.log(err));
-    }
-
-    this.allGamesRankings();
-
-    /*****************************************************
-    RANKINGS POR USUARIO
-    *****************************************************/
+    // RANKINGS POR USUARIO
     if (this.$store.getters.user != null) {
-      await this.createUsersRankings();
+      await this.getRankingsByUser();
       for (let x in this.userGamesRankings)
-        this.userRanking(this.gamesName[x], x);
+        this.ranking(this.gamesName[x], x, true);
     }
   },
   methods: {
-    allGamesRankings() {
-      for (let x in this.gamesName)
-        this.gameRanking( this.gamesName[x], x);   // Hay que rellenar en algún momento mongo con los datos de cada juego
+    async getGames() {
+      await fetch(`/games/`)
+        .then(res => {
+          return res.json();
+        })
+        .then(data => {
+          for (let x of data.response[0].allGames)
+            this.gamesName.push(x.name); 
+        })
+        .catch(err => console.log(err));
     },
-    gameRanking(name, index) {
-      let gamesRankings = document.getElementById("gamesRankings");
-      let gameTitle = document.createElement("h2")
-      gameTitle.textContent = name;
-      let table = document.createElement("table");
-      let tableHead = document.createElement("thead");
-      let tableBody = document.createElement("tbody");
-      let head = ["position", "nickname", "score", "time"];
-
-      let row = document.createElement("tr");
-      for (let i of head) {
-        let cell = document.createElement("td");
-        let text = document.createTextNode(i.toUpperCase());      
-        cell.appendChild(text);
-        row.appendChild(cell);
+    async getRankingsByGame() {
+      for (let y in this.gamesName) {
+        this.gamesRankings.push([]);
+        await fetch(`/rankings/${this.gamesName[y]}`)
+          .then(res => {
+            if (res.status != 200)  // Si recibe 404 es que no hay datos de ese ranking
+              return res.status;
+            return res.json();
+          })
+          .then(data => {
+            if (data == 404)
+              console.log("No hay ranking de este juego");
+            else {
+              for (let x of data.response[0].ranking) {
+                this.gamesRankings[y].push(x);
+              }
+            }
+          })
+        .catch(err => console.log(err));
       }
-      tableHead.appendChild(row);
-
-      for (let i = 0; i < this.numberOfGameRankingRows(index); i++) {
-        row = document.createElement("tr");
-
-        for (let j of head) {
-          let cell = document.createElement("td");
-          let content = j === "position" ? i + 1 : this.gamesRankings[index][i][j];
-          let text = document.createTextNode(content);      
-          
-          cell.appendChild(text);
-          row.appendChild(cell);
-        }
-        tableBody.appendChild(row);
-      }
-
-      table.appendChild(tableHead);
-      table.appendChild(tableBody);
-      gamesRankings.appendChild(gameTitle);
-      gamesRankings.appendChild(table);
     },
-    numberOfGameRankingRows(index) {
-      return this.gamesRankings[index].length < 10 ? this.gamesRankings[index].length : 10;
-    },
-    numberOfUserRankingRows(index) {
-      return this.userGamesRankings[index].length < 5 ? this.userGamesRankings[index].length : 5;
-    },
-    async createUsersRankings() {
+    async getRankingsByUser() {
       for (let y in this.gamesName) {
         this.userGamesRankings.push([]);
         await fetch(`/rankings/${this.$store.getters.user.nickname}&${this.gamesName[y]}`)
@@ -141,24 +89,22 @@ export default {
             if (data == 404)
               console.log("No hay ranking de este juego");
             else {
-              console.log(data);
               for (let x of data.response[0].ranking) {
                 this.userGamesRankings[y].push(x);
               }
-              console.log(this.userGamesRankings[y]);
             }
           })
           .catch(err => console.log(err));
       }
     },
-    userRanking(name, index) {
-      let gamesRankings = document.getElementById("userRankings");
+    ranking(name, index, user = false) {
+      let ranking = !user ? document.getElementById("gamesRankings") : document.getElementById("userRankings");
       let gameTitle = document.createElement("h2");
       gameTitle.textContent = name;
       let table = document.createElement("table");
       let tableHead = document.createElement("thead");
       let tableBody = document.createElement("tbody");
-      let head = ["position", "score", "time"];
+      let head = !user ? ["position", "nickname", "score", "time"] : ["position", "score", "time"];
 
       let row = document.createElement("tr");
       for (let i of head) {
@@ -169,13 +115,18 @@ export default {
       }
       tableHead.appendChild(row);
 
-      for (let i = 0; i < this.numberOfUserRankingRows(index); i++) {
+      let numberOfRows = !user ? this.numberOfGameRankingRows(index) : this.numberOfUserRankingRows(index);
+      for (let i = 0; i < numberOfRows; i++) {
         row = document.createElement("tr");
 
         for (let j of head) {
           let cell = document.createElement("td");
+          let content;
+          if (!user)
+            content = j === "position" ? i + 1 : this.gamesRankings[index][i][j];
+          else
+            content = j === "position" ? i + 1 : this.userGamesRankings[index][i][j];
 
-          let content = j === "position" ? i + 1 : this.userGamesRankings[index][i][j];
           let text = document.createTextNode(content);      
           
           cell.appendChild(text);
@@ -186,9 +137,15 @@ export default {
 
       table.appendChild(tableHead);
       table.appendChild(tableBody);
-      gamesRankings.appendChild(gameTitle);
-      gamesRankings.appendChild(table);      
-    }
+      ranking.appendChild(gameTitle);
+      ranking.appendChild(table);
+    },
+    numberOfGameRankingRows(index) {
+      return this.gamesRankings[index].length < 10 ? this.gamesRankings[index].length : 10;
+    },
+    numberOfUserRankingRows(index) {
+      return this.userGamesRankings[index].length < 5 ? this.userGamesRankings[index].length : 5;
+    },
   }
 }
 </script>
@@ -196,5 +153,9 @@ export default {
 <style scoped>
 .container {
   height: 1000px;
+}
+
+thead {
+  background: green;
 }
 </style>
